6b91cd9c1cc1e2911912e14f614672106bcfa82a
ltibbits000 <91166758+ltibbits000@users.noreply.github.com>
2021-12-08 18:53:05 -0800

    Project 2 Functions Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction () {
+    var sentence = "I am learning"; //assiging variable
+    sentence += "a lot from this book!";
+    document.getElementbyID("Concatenate").innerHTML = sentence;
+}
