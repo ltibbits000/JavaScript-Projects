9b8a8c7cab9d47ff6c65a4f675f8ba4614ac90ed
ltibbits000 <91166758+ltibbits000@users.noreply.github.com>
2021-12-08 17:32:49 -0800

    Basic  JS projects Submission assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+window.alert("Hello World");
+var words = "This is"; //Defining variable
+var words = words.fontcolor("blue");//assigning color 
+var morewords = " a sentence";
+var sentence = words + morewords;
+document.write(sentence);
+
+
