35e29d440cc778703180cdf8dde3037f6d150cda
ltibbits000 <91166758+ltibbits000@users.noreply.github.com>
2021-12-13 19:15:46 -0800

    Basic JavaScript Projects

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,62 +1,45 @@
-// Sum operator
-
+//Addition 
 function addition_Function() {
     var addition = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+    document.getElementById("sum").innerHTML= "2 + 2 = " + addition;
 }
 
-// Subtraction 
+//Subtraction
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("minuend").innerHTML= "5 - 2 = " + subtraction;
+}
 
-
-// Multiplication
-
+//Multiplication
 function multiplication() {
     var simple_Math = 6 * 8;
-    document.getElementById("Math").innerHTML = " 6 x 8 = " + simple_Math;
+    document.getElementById("product").innerHTML = " 8 * 6 = " + simple_Math;
 }
 
-// Division
-
-function division () {
-    var simple_Math = 48 / 6; 
-    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+//Division
+function division() {
+    var division = 48 / 6;
+    document.getElementById("quotient").innerHTML = "48 / 6 = " + division;
 }
 
-// Multiple operations
-
-function more_Math() {
-    var simple_Math = (1 + 2) * 10 / 2 -5;
-    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, 
-    divided in half and then subtracted by 5 equals " + simple_Math;
-}
-
-// Modulus operator
-
-function modulus_Operator() {
-    var simple_Math = 25 % 6;
-    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
-
-    )
-}
-
-// Unary operator 
-
-function negation_Operator () {
-    var x = 10;
-    document.getElementById("Math").innerHTML = -x;
-}
-
-//Increment and decrement operations
-
-var X = 5;
+// ++ 
+Var X = 5;
 X++;
 document.write(X);
 
-var Y = 5.25;
+// -- 
+Var Y = 5.25;
 Y--;
 document.write(Y);
 
-// Random operations
+//%
+function remainder() {
+    var remainder = 25 % 6;
+    document.getElementById("mod").innerHTML = "When you divide 25 by 6 you have a 
+    remainder of: " + remainder;
+}
 
+
+
+//random math
 window.alert(Math.random());
-
