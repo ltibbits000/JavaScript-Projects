0d86e85b38283658814e264e96efe1aad92ab0ba
ltibbits000 <91166758+ltibbits000@users.noreply.github.com>
2021-12-13 20:04:38 -0800

    Project 5

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,22 @@
+var A ="worm";
+document.write(typeof "worm" + 555);
+
+//===
+X= 11;
+Y= 11;
+document.write(X === Y);
+
+// ==
+(22+22)==
+document.write(33 == 33);
+
+// And operator
+document.write(5 > 2 && 10 > 4);
+
+// OR operator
+document.write( 5 > 10 || 10 > 20);
+
+//Not operator
+function not_Function() {
+    document.getElementById("Not").innerHTML = ! (5 > 10);
+}
