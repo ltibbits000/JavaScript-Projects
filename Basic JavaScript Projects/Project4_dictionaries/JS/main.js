ceeed7d504bc72a0a7ea93192409c5ee752f7c92
ltibbits000 <91166758+ltibbits000@users.noreply.github.com>
2021-12-15 18:43:04 -0600

    Project 4 dictionaries

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,11 @@
-function my_Dictionary() {
-    var Animal = {
+function my_Dictionary() { 
+    var Animal = {          //assigning variables
         Species: "Dog",
         Color: "Black",
         Breed: "Labrador",
         Age: 5,
         Sound: "Bark!"
     };
+    delete Animal.Sound;    //delete animal sound value 
     document.getElementById("Dictionary").innerHTML = Animal.Sound;
 }
